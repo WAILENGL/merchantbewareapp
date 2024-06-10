a8fdd0b576fbc66822cdbc019bbdb72abaab0dbe
WAILENGL <162903708+WAILENGL@users.noreply.github.com>
2024-06-11 00:11:55 +0800

    Feat: Added Toast to edit.jsx

--- web/frontend/pages/edit.jsx
+++ web/frontend/pages/edit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
 	Card,
 	Page,
@@ -7,14 +7,17 @@ import {
 	TextField,
 	Button,
 	TextContainer,
+	Frame,
+	Toast,
 } from '@shopify/polaris';
 import { TitleBar } from '@shopify/app-bridge-react';
 import { useTranslation } from 'react-i18next';
 import { useAuthenticatedFetch } from '../hooks';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 export default function ReportForm({ customerEmail, customerAddress }) {
 	const { t } = useTranslation();
+	const navigate = useNavigate();
 
 	const [notes, setNotes] = useState('');
 	const [customerInfo, setCustomerInfo] = useState({});
@@ -22,6 +25,9 @@ export default function ReportForm({ customerEmail, customerAddress }) {
 	const reasons = ['Chargeback', 'Unreasonable', 'Harassment', 'Others'];
 	const fetch = useAuthenticatedFetch();
 	const [reason, setReason] = useState('Chargeback');
+	const [toastActive, setToastActive] = useState(false);
+	const [toastMessage, setToastMessage] = useState('');
+	const toastDuration = 1000; // Duration in milliseconds
 
 	const userId = searchParams.get('userId');
 	const isEdit = searchParams.get('isEdit');
@@ -36,46 +42,64 @@ export default function ReportForm({ customerEmail, customerAddress }) {
 					content: notes,
 				}),
 			});
-			const result = await updateData.json();
-			console.log(result);
+			if (updateData.ok) {
+				setToastMessage('Report saved successfully');
+				setToastActive(true);
+			} else {
+				console.error('Failed to save report');
+			}
 		} catch (err) {
 			console.log(err);
 		}
 	};
 
 	const handleDelete = async () => {
-		const DeletedData = await fetch(
-			`/api/customers/report/${customerInfo?.report?.id}`,
-			{
-				method: 'DELETE',
+		try {
+			const DeletedData = await fetch(
+				`/api/customers/report/${customerInfo?.report?.id}`,
+				{
+					method: 'DELETE',
+				}
+			);
+			if (DeletedData.ok) {
+				setToastMessage('Report deleted successfully');
+				setToastActive(true);
+			} else {
+				console.error('Failed to delete report');
 			}
-		);
-		const result = await DeletedData.json();
-		console.log({ result });
-		console.log('Report deleted');
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
 	const handleUpdate = async () => {
-		const updateData = await fetch(
-			`/api/customers/report/${customerInfo?.report?._id}`,
-			{
-				method: 'PUT',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({
-					content: notes,
-					reason: reason,
-				}),
+		try {
+			const updateData = await fetch(
+				`/api/customers/report/${customerInfo?.report?._id}`,
+				{
+					method: 'PUT',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						content: notes,
+						reason: reason,
+					}),
+				}
+			);
+			if (updateData.ok) {
+				setToastMessage('Report updated successfully');
+				setToastActive(true);
+			} else {
+				console.error('Failed to update report');
 			}
-		);
-		const result = await updateData.json();
-		console.log({ result });
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
 	const editValue = isEdit === 'true';
 
 	const handleCancel = () => {
-		// Handle cancel action
-		console.log('Report canceled');
+		navigate('/customers');
 	};
 
 	async function fetchCustomers() {
@@ -95,87 +119,105 @@ export default function ReportForm({ customerEmail, customerAddress }) {
 		}
 	}
 
+	const toggleToastActive = useCallback(() => {
+		setToastActive((active) => !active);
+		setTimeout(() => {
+			navigate('/customers');
+		}, 100); // Delay to ensure Toast is dismissed before navigation
+	}, [navigate]);
+
 	useEffect(() => {
 		fetchCustomers();
 	}, [userId]);
 
+	const toastMarkup = toastActive ? (
+		<Toast
+			content={toastMessage}
+			onDismiss={toggleToastActive}
+			duration={toastDuration}
+		/>
+	) : null;
+
 	return (
-		<Page>
-			<TitleBar title={t('Bad Customer Report')} />
-			<Card sectioned>
-				<FormLayout>
-					<TextContainer>
-						<b>Customer Name:</b> {customerInfo?.first_name}{' '}
-						{customerInfo?.last_name}
-					</TextContainer>
-					<TextContainer>
-						<b>Customer Email:</b> {customerInfo?.email}
-					</TextContainer>
+		<Frame>
+			<Page>
+				<TitleBar title={t('Bad Customer Report')} />
+				<Card sectioned>
+					<FormLayout>
+						<TextContainer>
+							<b>Customer Name:</b> {customerInfo?.first_name}{' '}
+							{customerInfo?.last_name}
+						</TextContainer>
+						<TextContainer>
+							<b>Customer Email:</b> {customerInfo?.email}
+						</TextContainer>
 
-					<TextContainer>
-						<b>Customer Address: </b>
-						{customerInfo?.addresses && customerInfo.addresses.length > 0 ? (
-							<>
-								{customerInfo.addresses[0].address1}{' '}
-								{customerInfo.addresses[0].address2}{' '}
-								{customerInfo.addresses[0].province_code}{' '}
-								{customerInfo.addresses[0].zip}{' '}
-								{customerInfo.addresses[0].country}
-							</>
-						) : (
-							<span>No address available</span>
-						)}
-					</TextContainer>
-
-					<Select
-						value={reason}
-						label="Reason for report"
-						options={reasons.map((reason) => ({
-							label: reason,
-							value: reason,
-						}))}
-						onChange={setReason}
-					/>
-					<TextField
-						label="Notes"
-						placeholder="Enter notes here"
-						multiline={4}
-						value={notes}
-						onChange={setNotes}
-					/>
-					<FormLayout.Group>
-						{editValue && (
-							<div style={{ flex: 1 }}>
-								<Button secondary onClick={handleDelete}>
-									Delete Report
-								</Button>
-							</div>
-						)}
-
-						<div
-							style={{
-								display: 'flex',
-								justifyContent: 'flex-end',
-								width: '100%',
-							}}
-						>
-							<Button primary onClick={handleCancel} destructive>
-								Cancel
-							</Button>
-							<span style={{ margin: '0 8px' }}></span>
-							{!editValue ? (
-								<Button primary onClick={handleSave}>
-									Save
-								</Button>
+						<TextContainer>
+							<b>Customer Address: </b>
+							{customerInfo?.addresses && customerInfo.addresses.length > 0 ? (
+								<>
+									{customerInfo.addresses[0].address1}{' '}
+									{customerInfo.addresses[0].address2}{' '}
+									{customerInfo.addresses[0].province_code}{' '}
+									{customerInfo.addresses[0].zip}{' '}
+									{customerInfo.addresses[0].country}
+								</>
 							) : (
-								<Button primary onClick={handleUpdate}>
-									Update
-								</Button>
+								<span>No address available</span>
 							)}
-						</div>
-					</FormLayout.Group>
-				</FormLayout>
-			</Card>
-		</Page>
+						</TextContainer>
+
+						<Select
+							value={reason}
+							label="Reason for report"
+							options={reasons.map((reason) => ({
+								label: reason,
+								value: reason,
+							}))}
+							onChange={setReason}
+						/>
+						<TextField
+							label="Notes"
+							placeholder="Enter notes here"
+							multiline={4}
+							value={notes}
+							onChange={setNotes}
+						/>
+						<FormLayout.Group>
+							{editValue && (
+								<div style={{ flex: 1 }}>
+									<Button secondary onClick={handleDelete}>
+										Delete Report
+									</Button>
+								</div>
+							)}
+
+							<div
+								style={{
+									display: 'flex',
+									justifyContent: 'flex-end',
+									width: '100%',
+								}}
+							>
+								<Button primary onClick={handleCancel} destructive>
+									Cancel
+								</Button>
+								<span style={{ margin: '0 8px' }}></span>
+								{!editValue ? (
+									<Button primary onClick={handleSave}>
+										Save
+									</Button>
+								) : (
+									<Button primary onClick={handleUpdate}>
+										Update
+									</Button>
+								)}
+							</div>
+						</FormLayout.Group>
+					</FormLayout>
+				</Card>
+			</Page>
+			{toastMarkup}
+		</Frame>
 	);
 }
