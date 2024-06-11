117d630c1a44eb942b143716e525cb3f85a99eae
WAILENGL <162903708+WAILENGL@users.noreply.github.com>
2024-06-11 21:38:06 +0800

    removed redundant folder

--- web/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getOrders } from '../controllers/orderController.js';
-
-const router = express.Router();
-
-router.get('/', getOrders);
-
-export default router;
