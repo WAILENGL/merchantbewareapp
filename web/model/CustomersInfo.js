0e5cbec4509571ac7d2f758d9b25eb1e552ae7a6
WAILENGL <162903708+WAILENGL@users.noreply.github.com>
2024-06-12 18:27:07 +0800

    Feat: Restored reportschema under customerinfo

--- web/model/CustomersInfo.js
+++ web/model/CustomersInfo.js
@@ -1,26 +1,26 @@
 import mongoose from 'mongoose';
+import { reportSchema } from './ReportModel.js';
 
 const { Schema } = mongoose;
 
-
 const AddressSchema = new Schema({
-  id: Number,
-  customer_id: Number,
-  first_name: { type: String, default: null },
-  last_name: { type: String, default: null },
-  company: { type: String, default: null },
-  address1: String,
-  address2: { type: String, default: null },
-  city: String,
-  province: String,
-  country: String,
-  zip: String,
-  phone: String,
-  name: String,
-  province_code: String,
-  country_code: String,
-  country_name: String,
-  default: Boolean
+	id: Number,
+	customer_id: Number,
+	first_name: { type: String, default: null },
+	last_name: { type: String, default: null },
+	company: { type: String, default: null },
+	address1: String,
+	address2: { type: String, default: null },
+	city: String,
+	province: String,
+	country: String,
+	zip: String,
+	phone: String,
+	name: String,
+	province_code: String,
+	country_code: String,
+	country_name: String,
+	default: Boolean,
 });
 
 const CustomerSchema = new Schema(
@@ -29,8 +29,9 @@ const CustomerSchema = new Schema(
 		email: String,
 		first_name: String,
 		last_name: String,
-    addresses: [AddressSchema],
-		tags: { type: String, default: '' }
+		addresses: [AddressSchema],
+		tags: { type: String, default: '' },
+		reports: [reportSchema],
 	},
 	{ timeStamp: true }
 );
